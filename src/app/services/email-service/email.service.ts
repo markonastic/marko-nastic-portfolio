b847561b7e3f62afb092974ec73f252c76023f7d
Marko Nastic <marko.nastic@hotmail.com>
2020-02-19 00:56:56 +0100

    Added httpheader to POST request

--- src/app/services/email-service/email.service.ts
+++ src/app/services/email-service/email.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export class FormspreeResponse {
   ok: boolean;
@@ -13,12 +13,14 @@ export class EmailService {
   constructor(private http: HttpClient) { }
 
   sendEmail(name: string, email: string, message: string) {
+    const headers = new HttpHeaders().append('Content-Type', 'application/json');
     return this.http.post('https://formspree.io/mrgqonlg',
                             {
                               name,
                               email,
                               message
-                            }
+                            },
+                            { headers }
                          ).pipe();
   }
 }
