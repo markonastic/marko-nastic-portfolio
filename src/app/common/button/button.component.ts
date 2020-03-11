2cc7bc8b1b82627ebdc24b678c4a0f3666d08158
Marko Nastic <marko.nastic@hotmail.com>
2020-03-11 15:15:26 +0100

    Button component made full modular, edited components with button component implemented

--- src/app/common/button/button.component.ts
+++ src/app/common/button/button.component.ts
@@ -10,13 +10,7 @@ export class ButtonComponent {
 
   @Input() text: string;
   @Input() fontAwesome: string;
-  @Input() hover: boolean;
-  @Input() btnType = 'whiteAqua';
   @Input() hasBorder = false;
-  @Input() isBig = false;
-  @Input() isSquare = false;
-  @Input() fontSize: string;
-  @Input() onHover: string;
 
   constructor() { }
 }
